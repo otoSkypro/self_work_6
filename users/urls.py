e62bf0a5e3d54fc2947476d59646aa3950378b92
Семья <oc83@mail.ru>
2024-04-07 23:47:54 +0300

    23.1

--- users/urls.py
+++ users/urls.py
@@ -1,41 +1,29 @@
-# users/urls.py
-from users.views import RegisterView, verify_view, EmailVerifyView
-from .views import ProfileView, ProfileUpdateView
-from django.urls import path, reverse_lazy
-from users.apps import UsersConfig
-from django.contrib.auth.views import (
-    PasswordResetCompleteView,
-    PasswordResetConfirmView,
-    PasswordResetDoneView,
-    PasswordResetView,
-    LogoutView,
-    LoginView,
-)
+"""
+URL configuration for Django_Web_Store project.
 
-app_name = UsersConfig.name
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
 
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-
-    path('register/', RegisterView.as_view(), name='register'),
-    path('verify_email/', verify_view, name='verify_email'),
-    path('verify_email/<str:uidb64>/<str:user_token>/', EmailVerifyView.as_view(), name='incorrect_verify'),
-
-    path('password_reset/', PasswordResetView.as_view(template_name='users/password_reset.html',
-                                                      email_template_name='users/password_reset_email.html',
-                                                      success_url=reverse_lazy('users:password_reset_done')),
-         name='password_reset'),
-    path('password_reset/done/', PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
-         name='password_reset_done'),
-    path('password_reset/<uidb64>/<token>/',
-         PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html',
-                                          success_url=reverse_lazy('users:password_reset_complete')),
-         name='password_reset_confirm'),
-    path('password_reset/complete/',
-         PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
-         name='password_reset_complete'),
-    path('profile/', ProfileView.as_view(), name='profile'),
-    path('profile/update/', ProfileUpdateView.as_view(), name='profile_update'),
-]
+    path('admin/', admin.site.urls),
+    path('', include('catalog.urls', namespace='catalog')),
+    path('blogpost/', include('blogpost.urls', namespace='blogpost')),
+    path('users/', include('users.urls', namespace='users')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
